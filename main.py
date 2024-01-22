9a1563f7ed961361dd76bc0807f43aa2c0529777
BeafLee <carlos.chung.754@gmail.com>
2024-01-22 14:48:09 -0500

    Implementación de la api para descargar audio en mp3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, send_file
+from werkzeug.utils import secure_filename
 import os
-import io
 import moviepy.editor as mp
 from pytube import YouTube
 
@@ -18,25 +18,46 @@ def home():
 
 @app.route('/download/mp4', methods=['POST'])
 def mp4_downloader():
-    print("into")
+    option = request.form['btn']
+
     url = request.form['url']
-    print(url)
-
     video = YouTube(url)
+    
+    if option == 'mp4':            
+        print("option mp4")
+        
+        # Obtener la mejor resolución disponible
+        stream = video.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
 
-    # Obtener la mejor resolución disponible
-    stream = video.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
+        # Descargar el video
+        url_video = stream.download(path, 'video.mp4')
 
-    # Descargar el video
-    url_video = stream.download(path, 'video.mp4')
+        # Devolver el video como un archivo adjunto directamente al navegador
+        return send_file(
+            url_video,
+            as_attachment= True,
+            download_name= secure_filename(f"{video.title}.{option}"),
+            mimetype='video/mp4'
+        )
+    
+    elif option == 'mp3':
+        print('option mp3')
+        
+        # Obtener la mejor resolución disponible
+        stream = video.streams.filter(only_audio=True).first()
 
-    # Devolver el video como un archivo adjunto directamente al navegador
-    return send_file(
-        url_video,
-        as_attachment=True,
-        download_name=f"{video.title}.mp4",
-        mimetype='video/mp4'
-    )
+        # Descargar el video
+        url_video = stream.download(path, 'audio.mp3')
+
+        # Devolver el video como un archivo adjunto directamente al navegador
+        return send_file(
+            url_video,
+            as_attachment=True,
+            download_name= secure_filename(f"{video.title}.{option}"),
+            mimetype='audio/mp3'
+        )
+    
+
 
 
 
